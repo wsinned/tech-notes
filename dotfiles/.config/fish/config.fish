85a0346ce500efa4b47965272121ab286cc5d574
wsinned <wsinned@users.noreply.github.com>
2025-04-21 13:18:49 +0100

    Update config.fish
    Corrects formmating string and uses relative date for git log pretty

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -10,6 +10,6 @@ test -f /usr/share/ublue-os/bling/bling.fish && source /usr/share/ublue-os/bling
 abbr la eza -l --all --icons=auto --group-directories-first
 abbr g git
 abbr s git status -sbu
-abbr l git log --pretty=format:'"%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s"' --date=short --graph
+abbr l git log --pretty=format:'"%C(yellow)%h %C(reset)%ad %C(blue)%an%C(green)%d %C(reset)%s"' --date=relative --graph
 abbr ci git commit
 abbr ga git add --all
