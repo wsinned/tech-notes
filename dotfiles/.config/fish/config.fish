9c009567247f5ab1ab56fd6b04959744d1023135
Dennis Woodruff <denniswoodruffuk@gmail.com>
2025-04-21 13:47:39 +0100

    Moves abbreviations to dedicated file in conf.d

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -6,10 +6,3 @@ set -gx SHELL /usr/bin/fish
 ### bling.fish source start
 test -f /usr/share/ublue-os/bling/bling.fish && source /usr/share/ublue-os/bling/bling.fish
 ### bling.fish source end
-
-abbr la eza -l --all --icons=auto --group-directories-first
-abbr g git
-abbr s git status -sbu
-abbr l git log --pretty=format:'"%C(yellow)%h %C(reset)%ad %C(blue)%an%C(green)%d %C(reset)%s"' --date=relative --graph
-abbr ci git commit
-abbr ga git add --all
