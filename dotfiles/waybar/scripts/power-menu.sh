ff0c0d88d255f0834e1261aa4be106f5271874e1
Dennis Woodruff <denniswoodruffuk@gmail.com>
2026-01-28 08:27:59 +0000

    Moves power menu to vicinae

--- dotfiles/waybar/scripts/power-menu.sh
+++ dotfiles/waybar/scripts/power-menu.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-options="a. Lock\nb. Logout\nc. Reboot\nd. Shutdown\ne. Suspend\nf. Hibernate\nExit"
-chosen=$(echo -e "$options" | wofi --dmenu --prompt "Power Menu" --width 300 --height 234)
+options="Lock\nLogout\nReboot\nShutdown\nSuspend\nHibernate\nExit"
+chosen=$(echo -e "$options" | vicinae dmenu --placeholder "Power Menu" --width 300 --height 400)
 
 case $chosen in
-  "d. Shutdown") systemctl poweroff ;;
-  "c. Reboot") systemctl reboot ;;
-  "b. Logout") niri msg action quit --skip-confirmation ;;
-  "e. Suspend") systemctl suspend ;;
-  "f. Hibernate") systemctl hibernate ;;
-  "a. Lock") swaylock ;;
+  "Shutdown") systemctl poweroff ;;
+  "Reboot") systemctl reboot ;;
+  "Logout") niri msg action quit --skip-confirmation ;;
+  "Suspend") systemctl suspend ;;
+  "Hibernate") systemctl hibernate ;;
+  "Lock") swaylock ;;
   "Exit") exit 0 ;;
   *) exit 1 ;;
 esac
-
