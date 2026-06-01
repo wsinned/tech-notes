24a871182b7ebdc3911fd1926d14cfae1db8b8ff
Dennis Woodruff <denniswoodruffuk@gmail.com>
2026-06-01 09:01:53 +0100

    Moved nvim dotfiles to correct folder.

--- dotfiles/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Autosave on insert leave and text change
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+--   pattern = { "*" },
+--   command = "silent! wall",
+--   nested = true,
+-- })
--- dotfiles/.config/nvim/lua/plugins/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins/plugins.lua
@@ -108,19 +108,4 @@ return {{
             }
         }
     }
-}, {
-    "RedsXDD/neopywal.nvim",
-    name = "neopywal",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("neopywal").setup({
-            transparent_background = true,
-            use_palette = {
-                dark = "wallust",
-                light = "wallust"
-            }
-        })
-        vim.cmd.colorscheme("neopywal-dark")
-    end
 }}
